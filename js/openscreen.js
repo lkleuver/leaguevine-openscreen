e7764566ebff47510cf4b9f8fb3ea22e63e6b8ba
Les Kleuver <les.kleuver@gmail.com>
2012-06-11 21:29:51 +0200

    undo

--- js/openscreen.js
+++ js/openscreen.js
@@ -1,74 +1,56 @@
 define([
-  'router',
   'config',
   'controller/ui',
   'model/sync',
   'model/tournament'
-], function(Router, Config, UI, Sync, Tournament){
+], function(Config, UI, Sync, Tournament){
   
-  //var app_router;
+  var app_router;
   var tournaments = [];
-  var womenTournament = [];
-  var mixedTournament = [];
-  var openTournament = [];
   var loadCount = 0;
   
   var initialize = function(){
-    Sync.initialize(); 
-  
-    /*for(var i = 0, c = Config.tournaments.length; i < c; i++) {
-      var t = new Tournament(Config.tournaments[0]);
+    Sync.initialize();
+    
+    UI.showLoading(true);
+    for(var i = 0, c = Config.tournaments.length; i < c; i++) {
+      var t = new Tournament(Config.tournaments[i]);
       t.on('change', onTournamentLoadEvent);
       t.fetch();
       tournaments.push(t);
-    } //end for */
+    }
 
-    //create womenTournament object for json data in rounds
-    womenTournament = new Tournament(Config.tournaments[0]);
-    womenTournament.on('change', onTournamentLoadEvent);
-    mixedTournament = new Tournament(Config.tournaments[1]);
-    mixedTournament.on('change', onTournamentLoadEvent);
-    openTournament = new Tournament(Config.tournaments[2]);
-    openTournament.on('change', onTournamentLoadEvent);
 
-    //set Router aan om # op te vangen
-    Router.initialize(this);
-    //Router.on('change',onRouterChangeEvent);
+    app_router = new AppRouter();
+    Backbone.history.start();
   };
-
-  var fetchRound = function(division,round){
-    //fetch
-    //Hoe kun je iets mee sturen met fetch?
-    //zodat je uit de router een round nr en/of team-name kan meesturen
-    //UI.showLoading(true);
-    //womenTournament.fetch();
-    console.log("OPENSCREEN.fetchRound: " + division + " round: " + round);
-
-  }
-  var fetchTeam = function(division,team){
-
-
-  }
-
+  
   
   var onTournamentLoadEvent = function(e) {
-    console.log("onTournamentLoadEvent: " + JSON.stringify(e, null, 1));
-    //loadCount++;
-    //if(loadCount >= tournaments.length) {
+    loadCount++;
+    if(loadCount >= tournaments.length) {
       start();
-    //} //end if
+    }
   };
   
   var start = function() {
-    console.log("OPENSCREEN.START");
     UI.showLoading(false);
+    console.log("hooray");
     
-    //womenTournament.set("round",womenTournament.get("objects"));
-    console.log("GET ROUND 3: " + JSON.stringify(womenTournament.get("round"), null, 1));
-
+    console.log(tournaments[0].get("name"));
   };
+  
+  
 
+  var AppRouter = Backbone.Router.extend({
+    routes: {
+      '*actions': 'defaultAction'
+    },
 
+    defaultAction: function(actions){
+      
+    }
+  });
 
 
   return {
